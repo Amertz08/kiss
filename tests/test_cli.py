68d2edca8d5a558e119b4b4cdce69d2c8dee2631
Adam Mertz <adammertz@gmail.com>
2018-03-22 16:00:44 -0500

    Started CLI tests

---
+++ tests/test_cli.py
@@ -0,0 +1,15 @@
+import pytest
+from click.testing import CliRunner
+
+from kiss.cli import cli
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+def test_config_print_no_file(runner):
+    result = runner.invoke(cli, ['config'])
+    assert result.exit_code == 0, f'Invalid exit code: {result.exit_code}'
+    assert result.output == 'Missing .kiss.yml or not in a project directory'
