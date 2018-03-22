e22401a34d5c8353aedf08867160772c355feaf0
Adam Mertz <adammertz@gmail.com>
2018-03-22 11:59:06 -0500

    Added pyyaml to setup requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'click==6.7',
-        'jinja2==2.10'
+        'jinja2==2.10',
+        'pyyaml==3.12'
     ],
     entry_points='''
         [console_scripts]
