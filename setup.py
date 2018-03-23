26420ba7f4a4e56b58e9ce3fa4870312f8f999f4
Adam Mertz <adammertz@gmail.com>
2018-03-22 20:51:49 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 from setuptools import setup
 
+__version__ = '0.1'
+
+NAME = 'kiss'
+DESCRIPTION = 'Simple static site generator'
+URL = 'https://github.com/amertz08/kiss'
+AUTHOR = 'Adam Mertz'
+REQUIRES_PYTHON = '>=3.6.0'
+
 setup(
-    name='kiss',
-    version='0.1',
+    name=NAME,
+    version=__version__,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
     packages=['kiss'],
     include_package_data=True,
     install_requires=[
         'click==6.7',
         'jinja2==2.10',
         'pyyaml==3.12'
+    ],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
     ],
     entry_points='''
         [console_scripts]
